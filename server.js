8459c994b6d6cd8dc0c7a60e37263c6caac2266f
DPorch10 <aintshit@DPorch10s-MacBook-Pro.local>
2024-06-26 22:53:58 -0400

    push

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 // DEPENDENCIES
+require('dotenv').config()
 const express = require('express')
+const { Sequelize } = require('sequelize')
 const app = express()
 
-
 // CONFIGURATION / MIDDLEWARE
-require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+// DATABASE
+const { sequelize } = require('./models')
+const testSequelize = async () => {
+try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+// CONTROLLERS
+const bandsController = require('./controllers/bands_controller');
+app.use('/bands', bandsController)
 
 // ROOT
 app.get('/', (req, res) => {
@@ -17,17 +32,6 @@ app.get('/', (req, res) => {
 
 // LISTEN
 app.listen(process.env.PORT, () => {
+    testSequelize()
     console.log(`🎸 Rockin' on port: ${process.env.PORT}`)
 })
-
-// SEQUELIZE CONNECTION
-const sequelize = new Sequelize(process.env.PG_URI)
-
-try {
-    sequelize.authenticate() 
-    console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
-} catch(err) {
-    console.log(`Unable to connect to PG: ${err}`) 
-}
-
-
